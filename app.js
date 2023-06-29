64b0a3a40bec5c9f3f771a447fab65131a9de285
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-06-29 17:36:11 +0100

    set-up entry into server via port

---
+++ app.js
@@ -0,0 +1,33 @@
+const morgan = require('morgan');
+const express = require('express');
+const limoRouter = require('./routes/limoRouter');
+
+require('dotenv').config();
+
+const connectToDatabase = require('./server');
+const app = express();
+const bodyparser = require('body-parser');
+
+connectToDatabase();
+
+//  use morgan to view requests types
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(morgan('dev'));
+}
+
+app.use(express.static('public'));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Render the index page
+app.get('/xixuz', (req, res) => {
+  res.render('index', { data: { shortenedLimo: null, error: null } });
+});
+
+app.use('/xixuz/api/v1/', limoRouter);
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`app is listening on port : ${process.env.PORT}`);
+});
