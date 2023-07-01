0183e3b7b502627f169b1ea8a82ef05d43cd8ca1
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-07-01 12:07:01 +0100

    form handling & styling on client side

--- public/script.js
+++ public/script.js
@@ -1,3 +1,17 @@
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const btnSubmitSignupForm = document.querySelector('.btn--signup');
+const formError = document.querySelector('.form__error');
 
+const verifyPassword = (event) => {
+  event.preventDefault();
 
+  if (password.value !== password2.value) {
+    formError.classList.remove('hide');
+  } else {
+    formError.classList.add('hide');
+    btnSubmitSignupForm.removeEventListener('click', verifyPassword);
+  }
+};
 
+btnSubmitSignupForm.addEventListener('click', verifyPassword);
