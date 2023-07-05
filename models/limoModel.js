c7252975c0f5a4610dc9ef5cb735fa8ee64bd645
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2023-07-05 08:10:20 +0100

    Bug Fix: return date of host's env current locale

--- models/limoModel.js
+++ models/limoModel.js
@@ -22,18 +22,6 @@ const limoSchema = new mongoose.Schema({
   },
   date_created: {
     type: String,
-    default: function () {
-      const date = new Date();
-      const options = {
-        weekday: 'short',
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-      };
-      return date.toLocaleDateString('en-US', options);
-    },
   },
   limo_lastModifiedAt: Date,
   limo_history: {
@@ -49,6 +37,23 @@ const limoSchema = new mongoose.Schema({
 limoSchema.methods.updateAnalytics = async function () {
   this.analytics = this.analytics + 1;
 };
+
+limoSchema.pre('save', function (next) {
+  const date = new Date();
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const dayofWeek = daysOfWeek[String(date.getDay())];
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  const amOrpm = hours < 12 ? 'AM' : 'PM';
+
+  this.date_created = `${dayofWeek} ${day}/${month}/${year} ${hours}:${minutes}${amOrpm}`;
+
+  next();
+});
 
 limoSchema.pre('save', function (next) {
   if (!this.isModified('shortened_url')) return next();
